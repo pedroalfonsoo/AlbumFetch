763c3b1703ecc23efc02be71a1cfbd55444f1d9a
Pedro Alfonso <pedroalfonso24@hotmail.com>
2020-04-11 22:07:37 -0500

    Initial Commit

---
+++ AlbumFetch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AlbumFetch
+//
+//  Created by Pedro Alfonso on 4/11/20.
+//  Copyright © 2020 Pedro Alfonso. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
